243c284f57cb2871a4f8b7e111f4ab57c70b794c
Jeff Mesnil <jmesnil@gmail.com>
2014-02-14 17:49:54 +0100

    simplify motions.html

--- mqtt/ios/Motions/Motions/MWMViewController.h
+++ mqtt/ios/Motions/Motions/MWMViewController.h
@@ -2,7 +2,7 @@
 //  MWMViewController.h
 //  Motions
 //
-//  Created by Jeff Mesnil on 10/02/2014.
+//  Created by Jeff Mesnil on 14/02/2014.
 //  Copyright (c) 2014 Mobile & Web Messaging. All rights reserved.
 //
 
--- mqtt/ios/Motions/Motions/MWMViewController.m
+++ mqtt/ios/Motions/Motions/MWMViewController.m
@@ -2,29 +2,13 @@
 //  MWMViewController.m
 //  Motions
 //
-//  Created by Jeff Mesnil on 10/02/2014.
+//  Created by Jeff Mesnil on 14/02/2014.
 //  Copyright (c) 2014 Mobile & Web Messaging. All rights reserved.
 //
 
 #import "MWMViewController.h"
-#import <MQTTKit/MQTTKit.h>
-#import <CoreMotion/CoreMotion.h>
 
-#define kMqttHost @"iot.eclipse.org"
-#define kMotionTopic @"/mwm/%@/motion"
-#define kAlertTopic @"/mwm/%@/alert"
-
-@interface MWMViewController () <MQTTClientDelegate>
-
-@property (weak, nonatomic) IBOutlet UILabel *deviceIDLabel;
-@property (weak, nonatomic) IBOutlet UILabel *pitchLabel;
-@property (weak, nonatomic) IBOutlet UILabel *rollLabel;
-@property (weak, nonatomic) IBOutlet UILabel *yawLabel;
-
-@property (strong, nonatomic) MQTTClient *mqttClient;
-@property (strong, nonatomic) NSString *deviceID;
-
-@property (strong, nonatomic) CMMotionManager *motionManager;
+@interface MWMViewController ()
 
 @end
 
@@ -33,126 +17,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    self.deviceID = [UIDevice currentDevice].identifierForVendor.UUIDString;
-    NSLog(@"Client identifier is %@", self.deviceID);
-    self.deviceIDLabel.text = self.deviceID;
-    
-    self.motionManager = [[CMMotionManager alloc] init];
-    // use a frequency of circa 10Hz to get the device motion updates
-    self.motionManager.deviceMotionUpdateInterval = 0.1;
-    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-    [self.motionManager startDeviceMotionUpdatesToQueue:queue withHandler:^(CMDeviceMotion *motion, NSError *error) {
-        if(!error) {
-            [self send:motion.attitude];
-            dispatch_async(dispatch_get_main_queue(), ^{
-                self.pitchLabel.text = [NSString stringWithFormat:@"pitch: %.1f", motion.attitude.pitch];
-                self.rollLabel.text = [NSString stringWithFormat:@"roll: %.1f", motion.attitude.roll];
-                self.yawLabel.text = [NSString stringWithFormat:@"yaw: %.1f", motion.attitude.yaw];
-            });
-        }
-    }];
-    [self connect];
+	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)dealloc
+- (void)didReceiveMemoryWarning
 {
-    [self.motionManager stopDeviceMotionUpdates];
-    [self unsubscribe];
-    [self disconnect];
-}
-
-#pragma mark - MQTT actions
-
-- (void)connect
-{
-    self.mqttClient = [[MQTTClient alloc] initWithClientId:self.deviceID];
-    // Override point for customization after application launch.
-    self.mqttClient.delegate = self;
-    [self.mqttClient connectToHost:kMqttHost];
-}
-
-- (void)disconnect
-{
-    [self.mqttClient disconnect];
-}
-
-- (void)subscribe
-{
-    NSString *alertTopic = [NSString stringWithFormat:kAlertTopic, self.deviceID];
-    [self.mqttClient subscribe:alertTopic
-                       withQos:0];
-}
-
-- (void)unsubscribe
-{
-    NSString *alertTopic = [NSString stringWithFormat:kAlertTopic, self.deviceID];
-    [self.mqttClient unsubscribe:alertTopic];
-}
-
-- (void)send:(CMAttitude *)attitude
-{
-    uint64_t values[3] = { CFConvertDoubleHostToSwapped(attitude.pitch).v,
-        CFConvertDoubleHostToSwapped(attitude.roll).v,
-        CFConvertDoubleHostToSwapped(attitude.yaw).v};
-    NSData *data = [NSData dataWithBytes:&values length:sizeof(values)];
-    [self.mqttClient publishData:data
-                           toTopic:[NSString stringWithFormat:kMotionTopic, self.deviceID]
-                           withQos:0
-                            retain:NO];
-}
-
-#pragma mark - MQTTClientDelegate
-
-- (void)client:(MQTTClient *)client
-    didConnect:(NSUInteger)code
-{
-    // once connect, subscribe to the client's alerts topic
-    [self subscribe];
-}
-
-- (void)client:(MQTTClient *)client didReceiveMessage:(MQTTMessage *)message
-{
-    NSString *alertTopic = [NSString stringWithFormat:kAlertTopic, self.deviceID];
-    if ([alertTopic isEqualToString:message.topic]) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [self warnUser:message.payloadString];
-        });
-    }
-}
-
-# pragma mark - UI Actions
-
-// Warn the user by changing the view's background color to red for 2 seconds
-- (void)warnUser:(NSString *)colorStr
-{
-    // keep a reference to the original color
-    UIColor *originalColor = self.view.backgroundColor;
-    
-    [UIView animateWithDuration:0.5
-                          delay:0.0
-                        options:0
-                     animations:^{
-                         // change it to the color passed in parameter
-                         SEL sel = NSSelectorFromString([NSString stringWithFormat:@"%@Color", colorStr]);
-                         UIColor* color = nil;
-                         if ([UIColor respondsToSelector:sel]) {
-                             color  = [UIColor performSelector:sel];
-                         } else {
-                             color = [UIColor redColor];
-                         }
-                         self.view.backgroundColor = color;
-                     }
-                     completion:^(BOOL finished) {
-                         // after a delay of 2 seconds, revert it to the original color
-                         [UIView animateWithDuration:0.5
-                                               delay:2
-                                             options:0
-                                          animations:^{
-                                              self.view.backgroundColor = originalColor;
-                                          }
-                                          completion:nil];
-                     }];
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end
